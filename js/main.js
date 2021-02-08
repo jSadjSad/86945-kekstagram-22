700ce1785fc91722b6392fbfc64ad8ae7cb4d024
jSadjSad <ysadonina@yandex.ru>
2021-02-08 22:08:23 +0300

    Домашнее задание Module3-task1 5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const PHOTOS_AMOUNT = 25;
-const COMMENTATORS_AMOUNT = 6;
+const COMMENTATORS_AMOUNT = 10;
+const AVATARS_AMOUNT = 6;
 const MESSAGES_MAX_AMOUNT = 2;
-const MAX_COMMENTATOR_ID = 300;
+const MAX_COMMENTATOR_ID = 100;
 
 const PHOTO_DESCRIPTIONS = ['Пляж рядом с отелем', 'Go to the beach', 'Пляж с белым песочком', 'Девушка в бикини с фотоаппаратом', 'Прикольные рисовые человечки', 'Бэтмобиль',  'Клубника', 'Морс и ягодки',  'Самолет над пляжем', 'Галошница на колесиках', 'Дорога к морю', 'Белая AUDI посреди деревни', 'Странное блюдо из непонятно чего с офощами', 'Суши-кот', 'На диване в сапогах', 'Над облаками', 'Симфонический оркестр', 'Раритет', 'Тапки с подсветкой', 'Отель и пальмы', 'Вкусно и полезно', 'Закат над морем', 'Милашка-крабик', 'We will rock you', 'Дефендер и бегемоты'];
 
-const COMMMENTATORS_NAMES = ['Донна Роза Дальвадорес', 'Труфальдино', 'Фигаро', 'Тристан', 'Изольда', 'Кармен', 'Руслан', 'Людмила', 'Вахмурка', 'Гржимелик', 'Иван Бровкин', 'Йозеф Швейк', 'Шерлок Холмс', 'Джон Ватсон', 'Генри Баскервиль', 'ВанХельсинг', 'Влад Цепеш', 'Матроскин', 'Дядя Федор', 'Шарик', 'Игорь Иванович Печкин', 'Доктор Айболит', 'Бармалей', 'Джейсон Борн', 'Джеймс Бонд', 'Воландеморт'];
+const COMMMENTATORS_NAMES = ['Донна Роза Дальвадорес', 'Труфальдино', 'Фигаро', 'Руслан', 'Людмила', 'Вахмурка', 'Гржимелик', 'Шерлок Холмс', 'Джон Ватсон', 'Генри Баскервиль', 'ВанХельсинг', 'Влад Цепеш', 'Игорь Иванович Печкин', 'Доктор Айболит', 'Бармалей', 'Джейсон Борн', 'Джеймс Бонд', 'Воландеморт'];
 
 const COMMMENTATORS_MESSAGES = [ 'Всё отлично!',  'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
@@ -23,11 +24,10 @@ const createRandomNumbersArray = (n, min, max) => {
   let array = [];
   while (n > 0) {
     let randomNumber = getRandomIntInclusive(min, max);
-    // if (!array.includes(randomNumber)) {
-    if (!array.some(element => element == randomNumber)) {
+    if (!array.includes(randomNumber)) {
       array.push(randomNumber);
+      n--;
     }
-    n--;
   }
   return array;
 }
@@ -47,14 +47,21 @@ const createRandomArrayFromArray = (sourceArray, minAmount, maxAmount) => {
 }
 
 // Генерируем массив ID комментаторов
-const COMMMENTATORS_ID = createRandomNumbersArray(COMMMENTATORS_NAMES.length, 1, MAX_COMMENTATOR_ID);
+const COMMMENTATORS_IDS = createRandomNumbersArray(COMMMENTATORS_NAMES.length, 1, MAX_COMMENTATOR_ID);
 
 // Генерируем комментарий
-const createComment = (count) => {
+const createComment = () => {
   const randomNumber = getRandomIntInclusive(0, COMMMENTATORS_NAMES.length - 1);
+  const getAvatarNumber = (randomNumber) => {
+    if ((randomNumber + 1) <= (AVATARS_AMOUNT)) {
+      return randomNumber + 1;
+    }
+    return getAvatarNumber(randomNumber - AVATARS_AMOUNT);
+
+  }
   return {
-    id: COMMMENTATORS_ID[randomNumber],
-    avatar: 'img/avatar-' + count + '.svg',
+    id: COMMMENTATORS_IDS[randomNumber],
+    avatar: 'img/avatar-' + (getAvatarNumber(randomNumber)) + '.svg',
     message: (createRandomArrayFromArray(COMMMENTATORS_MESSAGES, 1, MESSAGES_MAX_AMOUNT)).join(' '),
     name: COMMMENTATORS_NAMES[randomNumber],
   }
